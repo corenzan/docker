aeec2fd2888613a331d1465a78d0e748dc33e7d8
Arthur <arthur@corenzan.com>
2022-05-28 11:57:37 -0300

    Update to PHP 8

--- wordpress/composer.sh
+++ wordpress/composer.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-expected_signature="$(curl -s https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer.php');"
-actual_signature="$(php -r "echo hash_file('SHA384', 'composer.php');")"
+expected_checksum="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+actual_checksum="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$expected_signature" != "$actual_signature" ]; then
-  >&2 echo 'ERROR: invalid installer signature'
-  rm composer.php
-  exit 1
+if test "$expected_checksum" != "$actual_checksum"; then
+	>&2 echo "$0: invalid checksum"
+	rm composer-setup.php
+	exit 1
 fi
 
-php composer.php --quiet
+php composer-setup.php --quiet
 result=$?
-rm composer.php
+rm composer-setup.php
 exit $result
